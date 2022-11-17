5a9778cdcdc9ff6751d55f5ec96307116ef18cdd
Khadija Shaikh <khadijah704shaikh@gmail.com>
2022-11-17 12:48:19 +0400

    Renaming file and adding 10 lesson details

---
+++ lessons.js
@@ -0,0 +1,112 @@
+let lessons =[
+    {
+        id: 1001,
+        subject:"Calculus",
+        location: "Leeds, United Kingdom",
+        description: "",
+        price: 350,
+        spaces: 5,
+        availableSpaces: 5,
+        image: "images/",
+        rating: 4
+      },
+      {
+        id: 1002,
+        subject:"Physical Education",
+        location: "Bournemouth, United Kingdom",
+        description: "",
+        price: 150,
+        spaces: 5,
+        availableSpaces: 5,
+        image: "images/",
+        rating: 4
+      },
+      {
+        id: 1003,
+        subject:"Introduction to Programming",
+        location: "Liverpool, United Kingdom",
+        description: "",
+        price: 650,
+        spaces: 5,
+        availableSpaces: 5,
+        image: "images/",
+        rating: 3
+      },
+      {
+        id: 1004,
+        subject:"Chemical Explosions",
+        location: "Norwich, United Kingdom",
+        description: "",
+        price: 550,
+        spaces: 5,
+        availableSpaces: 5,
+        image: "images/",
+        rating: 3
+      },
+      {
+        id: 1005,
+        subject:"Cyber Security",
+        location: "Darlington, United Kingdom",
+        description: "",
+        price: 375,
+        spaces: 5,
+        availableSpaces: 5,
+        image: "images/",
+        rating: 4
+      },
+      {
+        id: 1006,
+        subject:"Music Classes",
+        location: "Aberdeen, United Kingdom",
+        description: "",
+        price: 399,
+        spaces: 5,
+        availableSpaces: 5,
+        image: "images/",
+        rating: 2
+      },
+      {
+        id: 1007,
+        subject:"Introduction to Woodwork",
+        location: "Watford, United Kingdom",
+        description: "",
+        price: 200,
+        spaces: 5,
+        availableSpaces: 5,
+        image: "images/",
+        rating: 5
+      },
+      {
+        id: 1008,
+        subject:"Gymnastics",
+        location: "Wolverhampton, United Kingdom",
+        description: "",
+        price: 400,
+        spaces: 5,
+        availableSpaces: 5,
+        image: "images/",
+        rating: 4
+      },
+      {
+        id: 1009,
+        subject:"Graphics and Animation",
+        location: "Coventry, United Kingdom",
+        description: "",
+        price: 780,
+        spaces: 5,
+        availableSpaces: 5,
+        image: "images/",
+        rating: 3
+      },
+      {
+        id: 1010,
+        subject:"Photography 101",
+        location: "Stockport, United Kingdom",
+        description: "",
+        price: 395,
+        spaces: 5,
+        availableSpaces: 5,
+        image: "images/",
+        rating: 4
+      }
+]
